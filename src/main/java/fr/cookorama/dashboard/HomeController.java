96a652ba8260a8e67250cbe20571af4b67a2772a
Dan Sebag <dsebag@asptt.com>
2023-06-26 14:23:49 +0200

    update feature home

--- src/main/java/fr/cookorama/dashboard/HomeController.java
+++ src/main/java/fr/cookorama/dashboard/HomeController.java
@@ -1,14 +1,34 @@
 package fr.cookorama.dashboard;
 
 import fr.cookorama.dashboard.data.HomeData;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnEvents;
+
+    @FXML
+    private Button btnProduct;
+
+    @FXML
+    private Button btnExit;
 
     private String token;
 
@@ -16,8 +36,15 @@ public class HomeController implements Initializable {
         this.token = token;
     }
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources){
     }
+
+    @FXML
+    private void handleClicks(ActionEvent event){
+        if (event.getSource() == btnExit) {
+            System.exit(0);
+        }
+    }
+
 }
