e0fe3dd1793eee770112e64c2ac7a152fdd7896d
Dan Sebag <dsebag@asptt.com>
2023-06-26 11:33:37 +0200

    add app icon

--- src/main/java/fr/cookorama/dashboard/Main.java
+++ src/main/java/fr/cookorama/dashboard/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
@@ -14,7 +14,7 @@ public class Main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("connect.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
-
+        stage.getIcons().add(new Image("file:src/main/resources/fr/cookorama/dashboard/images/logo_tab.png"));
         stage.setTitle("Connexion");
         stage.setResizable(false);
         stage.setScene(scene);
