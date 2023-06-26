e21cd673ad01f9789c60f047ee7cb9b0ecd94aa5
Dan Sebag <dsebag@asptt.com>
2023-06-26 12:32:45 +0200

    update homecontroller

--- src/main/java/fr/cookorama/dashboard/HomeController.java
+++ src/main/java/fr/cookorama/dashboard/HomeController.java
@@ -1,19 +1,23 @@
 package fr.cookorama.dashboard;
 
+import fr.cookorama.dashboard.data.HomeData;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HomeController {
+public class HomeController implements Initializable {
 
     private String token;
 
-    public void initialize(URL location, ResourceBundle resources){
-
-        System.out.println("Token: " + token);
-
-    }
-
     public void setToken(String token) {
         this.token = token;
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
     }
 }
