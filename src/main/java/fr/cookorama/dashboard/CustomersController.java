adc4387e8a542048c0f9f7ebd53ab479616f6452
Dan Sebag <dsebag@asptt.com>
2023-06-27 12:56:02 +0200

    add chart exemple

--- src/main/java/fr/cookorama/dashboard/CustomersController.java
+++ src/main/java/fr/cookorama/dashboard/CustomersController.java
@@ -3,6 +3,8 @@ package fr.cookorama.dashboard;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,29 +17,26 @@ public class CustomersController extends HomeController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        PieChart pieChart = new PieChart();
-        // Ajoute 3 données en guise d'exemple
-        PieChart.Data slice1 = new PieChart.Data("Desktop", 213);
-        PieChart.Data slice2 = new PieChart.Data("Phone"  , 67);
-        PieChart.Data slice3 = new PieChart.Data("Tablet" , 36);
+        Tooltip tooltip = new Tooltip();
+        tooltip.setShowDelay(Duration.seconds(0.1));
 
-        pieChart.getData().add(slice1);
-        pieChart.getData().add(slice2);
-        pieChart.getData().add(slice3);
+        PieChart.Data slice1 = new PieChart.Data("Free", 5);
+        PieChart.Data slice2 = new PieChart.Data("Starter"  , 2);
+        PieChart.Data slice3 = new PieChart.Data("Master" , 1);
 
-        // Ajoute le graphique au layout
         customersChart.getData().add(slice1);
         customersChart.getData().add(slice2);
         customersChart.getData().add(slice3);
 
-        for (PieChart.Data data : pieChart.getData()) {
-            data.getNode().addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED, new javafx.event.EventHandler<javafx.scene.input.MouseEvent>() {
-                @Override
-                public void handle(javafx.scene.input.MouseEvent mouseEvent) {
-                    System.out.println("Clicked on " + data.getName());
-                }
-            });
+        for (PieChart.Data data : customersChart.getData()) {
+            data.getNode().addEventHandler(
+                    javafx.scene.input.MouseEvent.MOUSE_ENTERED,
+                    e -> {
+                        double percentage = (data.getPieValue() / customersChart.getData().stream().mapToDouble(PieChart.Data::getPieValue).sum()) * 100;
+                        tooltip.setText("Utilisateurs : " + String.format("%.2f", percentage) + "%");
+                        Tooltip.install(data.getNode(), tooltip);
+                    }
+            );
         }
-
     }
 }
