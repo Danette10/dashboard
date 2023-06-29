accf5ca7c8fef43b1e12edc11c9cc17b1ca7054a
Dan Sebag <dan.sebag1007@gmail.com>
2023-06-29 10:34:33 +0200

    add javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.fxml","l":"fr.cookorama.dashboard"},{"m":"com.fxml","l":"fr.cookorama.dashboard.data"}];updateSearchResults();
